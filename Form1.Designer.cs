6ae48752528c2a679f15a792468c75c36399c3f2
Tombibo <1525347023@qq.com>
2022-08-09 11:38:36 +0800

    第一次

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace PIE_LanTan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建地图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开地图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存地图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存地图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripTextBox2,
+            this.toolStripTextBox3,
+            this.toolStripTextBox4,
+            this.toolStripTextBox5});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1167, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建地图ToolStripMenuItem,
+            this.打开地图ToolStripMenuItem,
+            this.保存地图ToolStripMenuItem,
+            this.另存地图ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(53, 26);
+            this.toolStripTextBox1.Text = "文件";
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(53, 26);
+            this.toolStripTextBox2.Text = "编辑";
+            // 
+            // toolStripTextBox3
+            // 
+            this.toolStripTextBox3.Name = "toolStripTextBox3";
+            this.toolStripTextBox3.Size = new System.Drawing.Size(53, 26);
+            this.toolStripTextBox3.Text = "制图";
+            // 
+            // toolStripTextBox4
+            // 
+            this.toolStripTextBox4.Name = "toolStripTextBox4";
+            this.toolStripTextBox4.Size = new System.Drawing.Size(53, 26);
+            this.toolStripTextBox4.Text = "输出";
+            // 
+            // toolStripTextBox5
+            // 
+            this.toolStripTextBox5.Name = "toolStripTextBox5";
+            this.toolStripTextBox5.Size = new System.Drawing.Size(53, 26);
+            this.toolStripTextBox5.Text = "帮助";
+            // 
+            // 新建地图ToolStripMenuItem
+            // 
+            this.新建地图ToolStripMenuItem.Name = "新建地图ToolStripMenuItem";
+            this.新建地图ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.新建地图ToolStripMenuItem.Text = "新建地图";
+            // 
+            // 打开地图ToolStripMenuItem
+            // 
+            this.打开地图ToolStripMenuItem.Name = "打开地图ToolStripMenuItem";
+            this.打开地图ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.打开地图ToolStripMenuItem.Text = "打开地图";
+            // 
+            // 保存地图ToolStripMenuItem
+            // 
+            this.保存地图ToolStripMenuItem.Name = "保存地图ToolStripMenuItem";
+            this.保存地图ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.保存地图ToolStripMenuItem.Text = "保存地图";
+            // 
+            // 另存地图ToolStripMenuItem
+            // 
+            this.另存地图ToolStripMenuItem.Name = "另存地图ToolStripMenuItem";
+            this.另存地图ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.另存地图ToolStripMenuItem.Text = "另存地图";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1167, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton1.Text = "加载数据";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1167, 595);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem 新建地图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开地图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存地图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存地图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox5;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
